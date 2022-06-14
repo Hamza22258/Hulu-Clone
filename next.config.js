8f5c817fe79bc57b795973ab00e9ee479053c0d7
Hamza Malik <hamza.malik@devsinc.com>
2022-06-14 01:33:49 -0700

    Added frontend functionality and fetched from tmdb

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "links.papareact.com",
+      "img.tmdb.org",
+      "www.logo.wine",
+      "image.tmdb.org",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
